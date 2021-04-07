cdbf102c522490108dd3da65da9c705175fdb95a
ackho <ackho@uw.edu>
2021-04-07 00:12:11 -0700

    browser navbar

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Link from "next/link";
+import styled from 'styled-components'
+
+const StyledLink = styled.a`
+  color: white;
+  font-family: 'Lato', sans-serif;
+  text-decoration: none;
+  margin: 1em;
+`
+
+function NavLink({ href, name }) {
+  return (
+    <Link href={href} passHref>
+      <StyledLink>{name}</StyledLink>
+    </Link>
+  )
+}
 
 const Header: React.FC<{}> = (props) => {
   return (
@@ -11,11 +27,24 @@ const Header: React.FC<{}> = (props) => {
         padding: "1em",
         margin: "0em",
         width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
       <Link href="/">
-        <img src="/header-logo.png" style={{ cursor: "pointer" }} />
+        <img src="/header-logo.png" style={{
+                                            cursor: "pointer",
+                                            width: "26em",
+                                            padding: "1em"
+                                            }} />
       </Link>
+      <div style={{ marginRight: "3em" }}>
+        <NavLink href="/" name="Home"/>
+        <NavLink href="/" name="About"/>
+        <NavLink href="/" name="Training"/>
+        <NavLink href="/" name="Resources"/>
+        <NavLink href="/" name="Donations"/>
+      </div>
     </div>
   );
 };
