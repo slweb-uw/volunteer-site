00c05909caee58a6ae250cbc82f28e76c3eaa7c2
Bryan Lim <bryanlim@cs.washington.edu>
2021-04-12 15:08:56 -0700

    Improved 404 Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,19 @@
-import { useRouter } from "next/dist/client/router";
-import { useEffect } from "react";
+import { Button, Typography } from "@material-ui/core";
+import Link from "next/link";
+import React from "react";
 
 // pages/404.js
 export default function Custom404() {
-  return <div />;
+  return (
+    <div style={{ minHeight: 1000, padding: "3em", textAlign: "center" }}>
+      <Typography variant="h4">Error: Page Not Found</Typography>
+      <div style={{ marginTop: "3em" }}>
+        <Link href="/">
+          <Button color="secondary" variant="contained">
+            Return Home
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
