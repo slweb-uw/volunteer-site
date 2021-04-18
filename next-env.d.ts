07e9bd895d1987a057153635c6804037dd2478ce
taru <jkz8889@gmail.com>
2021-04-17 20:58:09 -0700

    add doc for recurence field in calendareventdata interface

--- next-env.d.ts
+++ next-env.d.ts
@@ -20,5 +20,12 @@ interface CalendarEventData {
   StartDate: string;    // format according to RFC5545, use toISOString() before send the api request
   EndDate: string;      // format according to RFC5545, use toISOString() before send the api request
   Timezone: string;     // Formatted as an IANA Time Zone Database name, e.g. "Europe/Zurich"
-  Recurrence?: string[]; // format according to RFC5545
+  // format according to 
+  // RFC5545(https://tools.ietf.org/html/rfc5545#page-37,
+  // https://tools.ietf.org/html/rfc5545#section-3.8.5)
+  // Date example(UTC): 20110617T065959Z
+  // The above indicates: Year:2011, Month: 06, Day: 17, Hour: 06(range: 00-23), Minite: 59, Second: 59
+  // recurrence example: ["RRULE:FREQ=WEEKLY;BYDAY:TU,SU;UNTIL=20110617T065959Z"], This means repeat weekly(Tuesday and Sunday) until the above time.
+  // Note that DTSTART and DTEND lines are not allowed in this field.
+  Recurrence?: string[]; 
 }
