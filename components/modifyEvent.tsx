908c1fa7872982d21c5e240874f95b35d6b6c3de
taru <jkz8889@gmail.com>
2021-04-18 19:54:46 -0700

    change error message display

--- components/modifyEvent.tsx
+++ components/modifyEvent.tsx
@@ -154,7 +154,7 @@ const CalendarModifyEvent: React.FC<Props> = (Props) => {
                 },
                 (error) => {
                     setIsLoaded(true);
-                    setError(error);
+                    setError(error.message);
                 }
             )
         }, [])
@@ -173,7 +173,7 @@ const CalendarModifyEvent: React.FC<Props> = (Props) => {
         <DialogTitle id="calendar-event-dialog">{"Calendar Event: " + calendarEvent.Name}</DialogTitle>
         <DialogContent>
           <DialogContentText id="calendar-event-description">
-              Error: {error?.['message']}
+              Error: {error}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
