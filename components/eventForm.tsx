2a921f0f739e97e1cb61ce34680b689ff7c5f6db
taru <jkz8889@gmail.com>
2021-06-14 19:21:00 -0700

    fix some conflicts issue

--- components/eventForm.tsx
+++ components/eventForm.tsx
@@ -5,7 +5,6 @@ import {
   Modal,
   Form,
   Input,
-  Radio,
   Col,
   Row,
   DatePicker,
@@ -39,15 +38,6 @@ interface Props {
   onCancel: any;
 }
 
-const getOrganizations = async (city) => {
-    try {
-        let doc = await firebaseClient.firestore().collection('cache').doc(city).get()
-        return doc.data();
-    } catch (e) {
-        console.log(e);
-    }
-    return null;
-}
 const getOrganizations = async (city: string) => {
   try {
     let doc = await firebaseClient
@@ -74,100 +64,17 @@ const generateLabelValuePairs = (
 };
 
 const CollectionCreateForm: React.FC<Props> = (Props) => {
-    const [form] = Form.useForm();
-    const [recEndDate, setRecEndDate] = useState(null);
-    const [tabNum, setTabNum] = useState(1);
-    const [interval, setInterval] = useState(null);
-    const [weekday, setWeekday] = useState(null);
-    const [month, setMonth] = useState(null);
-    const [monthday, setMonthday] = useState(null);
-    const [add, setAdd] = useState(false);
-    const [city, setCity] = useState(null);
-    const [orgo, setOrgo] = useState(null);
-
-    const weekdayOptions = [
-        { label: "Monday", value: "MO" },
-        { label: "Tuesday", value: "TU" },
-        { label: "Wednesday", value: "WE" },
-        { label: "Thursday", value: "TH" },
-        { label: "Friday", value: "FR" },
-        { label: "Saturday", value: "SA" },
-        { label: "Sunday", value: "SU" }
-    ];
-    const monthOptions = [
-        { label: "1", value: "1" },
-        { label: "2", value: "2" },
-        { label: "3", value: "3" },
-        { label: "4", value: "4" },
-        { label: "5", value: "5" },
-        { label: "6", value: "6" },
-        { label: "7", value: "7" },
-        { label: "8", value: "8" },
-        { label: "9", value: "9" },
-        { label: "10", value: "10" },
-        { label: "11", value: "11" },
-        { label: "12", value: "12" },
-    ];
-    const monthdayOptions = [
-        { label: "1", value: "1" },
-        { label: "2", value: "2" },
-        { label: "3", value: "3" },
-        { label: "4", value: "4" },
-        { label: "5", value: "5" },
-        { label: "6", value: "6" },
-        { label: "7", value: "7" },
-        { label: "8", value: "8" },
-        { label: "9", value: "9" },
-        { label: "10", value: "10" },
-        { label: "11", value: "11" },
-        { label: "12", value: "12" },
-        { label: "13", value: "13" },
-        { label: "14", value: "14" },
-        { label: "15", value: "15" },
-        { label: "16", value: "16" },
-        { label: "17", value: "17" },
-        { label: "18", value: "18" },
-        { label: "19", value: "19" },
-        { label: "20", value: "20" },
-        { label: "21", value: "21" },
-        { label: "22", value: "22" },
-        { label: "23", value: "23" },
-        { label: "24", value: "24" },
-        { label: "25", value: "25" },
-        { label: "26", value: "26" },
-        { label: "27", value: "27" },
-        { label: "28", value: "28" },
-        { label: "29", value: "29" },
-        { label: "30", value: "30" },
-        { label: "31", value: "31" }
-    ];
-
-    React.useEffect(() => {
-        let recurrencesFields = form.getFieldValue("recurrences");
-        if (add && (recEndDate != null)) {
-            let ind = recurrencesFields.length - 1
-            recurrencesFields[ind] = {
-                    recEndDate,
-                    tabNum,
-                    interval,
-                    weekday,
-                    month,
-                    monthday
-            }
-            setRecEndDate(null);
-        }
-      }, [add, recEndDate]);
-        form.setFieldsValue({
-            Recurrence: {
   const [form] = Form.useForm();
   const [recEndDate, setRecEndDate] = useState(null);
-  const [tabNum, setTabNum] = useState(1);
+  const [tabNum, setTabNum] = useState("1");
   const [interval, setInterval] = useState(null);
   const [weekday, setWeekday] = useState(null);
   const [month, setMonth] = useState(null);
   const [monthday, setMonthday] = useState(null);
+  const [add, setAdd] = useState(false);
   const [city, setCity] = useState(null);
   const [orgo, setOrgo] = useState(null);
+  const [volunteer, setVolunteer] = useState(null);
 
   const weekdayOptions = [
     { label: "Monday", value: "MO" },
@@ -180,20 +87,33 @@ const CollectionCreateForm: React.FC<Props> = (Props) => {
   ];
   const monthOptions = generateLabelValuePairs(12);
   const monthdayOptions = generateLabelValuePairs(31);
+
+  React.useEffect(() => {
+    let recurrencesFields = form.getFieldValue("recurrences");
+    if (add && (recEndDate != null)) {
+        let ind = recurrencesFields.length - 1
+        recurrencesFields[ind] = {
+                recEndDate,
+                tabNum,
+                interval,
+                weekday,
+                month,
+                monthday
+        }
+        setRecEndDate(null);
+    }
+  }, [add, recEndDate]);
+
+
   React.useEffect(() => {
     form.setFieldsValue({
-      Recurrence: {
-        recEndDate,
-        tabNum,
-        interval,
-        weekday,
-        month,
-        monthday,
+    Location: [
         city,
         orgo,
-      },
+        volunteer
+    ]
     });
-  }, [recEndDate, tabNum, interval, weekday, month, monthday, city, orgo]);
+  }, [city, orgo, volunteer]);
 
   return (
     <Modal
@@ -210,12 +130,13 @@ const CollectionCreateForm: React.FC<Props> = (Props) => {
             form.resetFields();
             Props.onCreate(values);
             setRecEndDate(null);
-            setTabNum(1);
+            setTabNum("1");
             setInterval(null);
             setWeekday(null);
             setMonth(null);
             setMonthday(null);
             setCity(null);
+            setAdd(false);
           })
           .catch((info) => {
             console.log("Validate Failed:", info);
@@ -286,7 +207,6 @@ const CollectionCreateForm: React.FC<Props> = (Props) => {
               name="Types of Volunteers Needed"
               label="Types of Volunteers Needed"
             >
-              <Input />
               <VolunteerType setVolunteer={setVolunteer} />
             </Form.Item>
 
@@ -422,7 +342,8 @@ const CollectionCreateForm: React.FC<Props> = (Props) => {
                                 <Select.Option value="1">1</Select.Option>
                                 <Select.Option value="2">2</Select.Option>
                                 <Select.Option value="3">3</Select.Option>
-                                <Select.Option value="4">4</Select.Option>
+                                <Select.Option value="4">4</Select.Option>:
+￼
                                 <Select.Option value="5">5</Select.Option>
                                 <Select.Option value="6">6</Select.Option>
                             </Select>
@@ -470,293 +391,17 @@ const CollectionCreateForm: React.FC<Props> = (Props) => {
                             </Space>
                         </TabPane>
                     </Tabs>
+                    <MinusCircleOutlined onClick={() => remove(name)} />
                 </Form.Item>
-                </Col>
-                <Col span={12}>
-                <Form.Item name="VolunteerType" label="Volunteer Type" >
-                    <Input onChange={ (v: any)=>{console.log(interval); } }/>
+              ))}
+                <Form.Item>
+                    <Button type="dashed" onClick={() => {add(); setAdd(true);}} block icon={<PlusOutlined />}>
+                        Add Recurrence
+                    </Button>
                 </Form.Item>
-                                </Space>
-                            </TabPane>
-                        </Tabs>
-                        <MinusCircleOutlined onClick={() => remove(name)} />
-                    </Form.Item>
-                 ))}
-                    <Form.Item>
-                        <Button type="dashed" onClick={() => {add(); setAdd(true);}} block icon={<PlusOutlined />}>
-                            Add Recurrence
-                        </Button>
-                    </Form.Item>
-               </>
-                )}   
-                </Form.List>
-                        {/*
-                <Form.List name="recurrences">
-                    {(fields, { add, remove }, { errors }) => (
-                    <>
-                        {fields.map((field) => (
-                      if (values.recurrences.recEndDate) {
-            let rec = [];
-            switch(values.Recurrence.tabNum) {
-                case "1":
-                    rec.push(`RRULE:FREQ=DAILY;INTERVAL=${values.Recurrence.interval};UNTIL=${values.Recurrence.recEndDate}`);
-                    break;
-                case "2":
-                    rec.push(`RRULE:FREQ=WEEKLY;BYDAY=${values.Recurrence.weekday};UNTIL=${values.Recurrence.recEndDate}`);
-                    break;
-                case "3":
-                    rec.push(`RRULE:FREQ=MONTHLY;BYMONTHDAY=${values.Recurrence.monthday};UNTIL=${values.Recurrence.recEndDate}`);
-                    break;
-                case "4":
-                    rec.push(`RRULE:FREQ=YEARLY;BYMONTH=${values.Recurrence.month};BYMONTHDAY=${values.Recurrence.monthday};UNTIL=${values.Recurrence.recEndDate}`);
-                    break;
-                default:
-                    break;
-           <Form.Item
-                            required={false}
-                            key={field.key}
-                            label="test"
-                        >
-                            <Form.Item
-                            
-                            >
-                            <Tabs>
-
-                            </Tabs>
-                            </Form.Item>
-                            <MinusCircleOutlined
-                                onClick={() => remove(field.name)}
-                            />
-                        </Form.Item>
-                        ))}
-                        <Form.Item>
-                        <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                            Add field
-                        </Button>
-                        </Form.Item>
-                    </>
-                    )}
-                </Form.List>
-                        */}
-                <Form.List name="users">
-                    {(fields, { add, remove }) => (
-                        <>
-                            {fields.map(({ key, name, fieldKey, ...restField }) => (
-                                <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
-                               onthly
-                            <Form.Item
-                                        {...restField}
-                                        name={[name, 'detailKey']}
-                                        fieldKey={[fieldKey, 'detailKey']}
-                                        rules={[{ required: true, message: 'Missing detail name' }]}
-                                    >
-                                        <Input placeholder="Detail Name (i.e. Parking Directions)" />
-                                    </Form.Item>
-                                    <Form.Item
-                                        {...restField}
-                                        name={[name, 'detailValue']}
-                                        fieldKey={[fieldKey, 'detailValue']}
-                                        rules={[{ required: true, message: 'Missing detail value' }]}
-                                    >
-                                        <Input placeholder="Detail Value (i.e. Park at level 2)" />
-                                    </Form.Item>
-                                    <MinusCircleOutlined onClick={() => remove(name)} />
-                                </Space>
-                            ))}
-                            <Form.Item>
-                                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                                    Add field
-                                </Button>
-                            </Form.Item>
-                        </>
-                    )}
-                </Form.List>
-
-                <Form.Item name="Field Ordering" label="Field Ordering" >
-                    {/* <Ordering /> */}
-                </Form.Item>
-                {/* <Form.Item name="Field Ordering" label="Field Ordering" >
-                    <Input onChange={(v: any) => console.log(orgo) }/>
-                </Form.Item> */}
-
-            </Col>
-            </Row>
-            </Form>
-        </Modal>
-    );
-};
-
-const CollectionsPage = () => {
-    const [visible, setVisible] = useState(false);
-    const DOMAIN = "http:" + '//' + "localhost:3000";
-    const calendarApiPath = '/api/put-calendar-event';
-    const eventApiPath = '/api/put-event-data';
-
-    const onCreate = (values) => {
-        const rangeTimeValue = values['DateObject'];
-        // console.log('Received values of form: ', values);
-
-
-        let startTime = rangeTimeValue[0].toISOString();
-        let endTime = rangeTimeValue[1].toISOString();
-        console.log(startTime);
-        console.log(endTime);
-
-        const calEvent: CalendarEventData = {
-            Name: values.Name,
-            Description: values.Description,
-            Organization: values.Organization,
-            Location: values.Location,
-            StartDate: startTime,
-            EndDate: endTime,
-            Timezone: "GMT"
-        }
-       
-        let rec = [];
-        for (let i of values.recurrences) {
-            if (i !== undefined && i.recEndDate) {
-                switch(i.tabNum) {
-                    case "1":
-                        rec.push(`RRULE:FREQ=DAILY;INTERVAL=${i.interval};UNTIL=${i.recEndDate}`);
-                        break;
-                    case "2":
-                        rec.push(`RRULE:FREQ=WEEKLY;BYDAY=${i.weekday};UNTIL=${i.recEndDate}`);
-                        break;
-                    case "3":
-                        rec.push(`RRULE:FREQ=MONTHLY;BYMONTHDAY=${i.monthday};UNTIL=${i.recEndDate}`);
-                        break;
-                    case "4":
-                        rec.push(`RRULE:FREQ=YEARLY;BYMONTH=${i.month};BYMONTHDAY=${i.monthday};UNTIL=${i.recEndDate}`);
-                        break;
-                    default:
-                        break;
-            }
-        }
-            calEvent.Recurrence = rec;
-        }
-
-        const firestoreEvent: CalendarEventData = {}
-        Object.keys(values).forEach((element) => {
-            if (element != "Recurrence" && element != "DateObject" && element != "addNewFields" && element != "Organization") {
-                firestoreEvent[element] = values[element];
-            }
-            if (element == "addNewFields" && values[element] != null) {
-                values[element].forEach(newField => {
-                        firestoreEvent[newField.detailKey] = newField.detailValue;
-                    })
-            }
-            if (element == "Organization") {
-                firestoreEvent[element] = values.Recurrence["orgo"];
-            }
-        });
-
-        Promise.all([
-            fetch(DOMAIN + calendarApiPath, {
-                method: 'POST',
-                body: JSON.stringify({ eventData: calEvent }),
-            }),
-            fetch(DOMAIN + eventApiPath, {
-                method: 'POST',
-                body: JSON.stringify({ eventData: calEvent }),
-            })
-        ]).then((res: any) => {
-              }}
-            >
-              <Tabs defaultActiveKey="1" onChange={(v: any) => setTabNum(v)}>
-                <TabPane tab="Daily" key="1">
-                  <Space>
-                    Reapeat Every
-                    <Select
-                      style={{ width: 120 }}
-                      onChange={(v: any) => setInterval(v)}
-                    >
-                      <Select.Option value="1">1</Select.Option>
-                      <Select.Option value="2">2</Select.Option>
-                      <Select.Option value="3">3</Select.Option>
-                      <Select.Option value="4">4</Select.Option>
-                      <Select.Option value="5">5</Select.Option>
-                      <Select.Option value="6">6</Select.Option>
-                    </Select>
-                    Day
-                  </Space>
-                  <br />
-                  <br />
-                  <Space>
-                    Recurrence End Date:
-                    <DatePicker
-                      showTime
-                      format="YYYY-MM-DD HH:mm:ss"
-                      onOk={(v: any) =>
-                        setRecEndDate(v.format("YYYYMMDD[T]HHmmss[Z]"))
-                      }
-                    />
-                  </Space>
-                </TabPane>
-                <TabPane tab="Weekly" key="2">
-                  <Checkbox.Group
-                    options={weekdayOptions}
-                    onChange={(v: any) => setWeekday(v)}
-                  />
-                  <br />
-                  <br />
-                  <Space>
-                    Recurrence End Date:
-                    <DatePicker
-                      showTime
-                      format="YYYY-MM-DD HH:mm:ss"
-                      onOk={(v: any) =>
-                        setRecEndDate(v.format("YYYYMMDD[T]HHmmss[Z]"))
-                      }
-                    />
-                  </Space>
-                </TabPane>
-                <TabPane tab="Monthly" key="3">
-                  Month day:
-                  <Checkbox.Group
-                    options={monthdayOptions}
-                    onChange={(v: any) => setMonthday(v)}
-                  />
-                  <br />
-                  <br />
-                  <Space>
-                    Recurrence End Date:
-                    <DatePicker
-                      showTime
-                      format="YYYY-MM-DD HH:mm:ss"
-                      onOk={(v: any) =>
-                        setRecEndDate(v.format("YYYYMMDD[T]HHmmss[Z]"))
-                      }
-                    />
-                  </Space>
-                </TabPane>
-                <TabPane tab="Yearly" key="4">
-                  Month:
-                  <Checkbox.Group
-                    options={monthOptions}
-                    onChange={(v: any) => setMonth(v)}
-                  />
-                  <br />
-                  <br />
-                  Month day:
-                  <Checkbox.Group
-                    options={monthdayOptions}
-                    onChange={(v: any) => setMonthday(v)}
-                  />
-                  <br />
-                  <br />
-                  <Space>
-                    Recurrence End Date:
-                    <DatePicker
-                      showTime
-                      format="YYYY-MM-DD HH:mm:ss"
-                      onOk={(v: any) =>
-                        setRecEndDate(v.format("YYYYMMDD[T]HHmmss[Z]"))
-                      }
-                    />
-                  </Space>
-                </TabPane>
-              </Tabs>
-            </Form.Item>
+            </>
+            )}   
+            </Form.List>
           </Col>
         </Row>
       </Form>
@@ -831,7 +476,10 @@ const CollectionsPage = () => {
       if (
         // element != "DateObject" &&
         element != "addNewFields" &&
-        element != "Organization"
+        element != "Organization" &&
+        element != "Location" &&
+        element != "recurrences" &&
+        element != "Types of Volunteers Needed"
       ) {
         firestoreEvent[element] = values[element];
       }
