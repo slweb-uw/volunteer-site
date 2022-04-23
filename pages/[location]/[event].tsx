eeb701038b9c993013a0a2fc2379fd74649dfcfb
Ryan Langford <ryanclangford@icloud.com>
2022-04-22 17:49:12 -0700

    removed empty fields from event details

--- pages/[location]/[event].tsx
+++ pages/[location]/[event].tsx
@@ -108,7 +108,8 @@ const Event: NextPage<Props> = ({ classes }) => {
     setEventData(next.data() as EventData);
   };
 
-  // console.log(eventData);
+  console.log("eventData: ", eventData);
+
 
   const options = { year: "numeric", month: "long", day: "numeric" };
 
@@ -125,6 +126,7 @@ const Event: NextPage<Props> = ({ classes }) => {
         parentURL={"/location/" + location}
         crumbs={["Opportunities in " + location, eventData.Title]}
       />
+      {/* EVENT TITLE */}
       <Typography variant="h5" style={{ fontWeight: 900, paddingBottom: 50 }}>
         {eventData?.Title}
       </Typography>
@@ -197,13 +199,15 @@ const Event: NextPage<Props> = ({ classes }) => {
 
       <Divider style={{ marginBottom: "3em", marginTop: "3em", height: 3, borderRadius: "25px"}}></Divider>
 
+      {/* Event page fields */}
       <Grid container spacing={4}>
         <EventField name="Project Description" value={<EventDescription event={eventData} />} />
-        {initialGridKeys.map((name) => (
+        {initialGridKeys.filter((name) => eventData[name] != null && eventData[name] != "").map((name) => (
           <EventField key={name} name={name} value={eventData[name]} />
         ))}
         {Object.keys(eventData)
           .filter((name) => !reservedKeys.includes(name))
+          .filter((name) => eventData[name] != null && eventData[name] != "")
           .map((name) => (
             <EventField key={name} name={name} value={eventData[name]} />
           ))}
@@ -237,6 +241,9 @@ const styles = createStyles({
     minHeight: "100%",
     borderRadius: "10px",
     objectFit: "cover",
+  },
+  hidden: {
+    display: "none",
   },
 });
 export default withStyles(styles)(Event);
