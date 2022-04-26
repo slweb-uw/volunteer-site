5618edf111dd0117e5ff51a3ac123a4b04c3ff0e
Chandra Burnham <burnhc@cs.washington.edu>
2022-04-25 19:37:20 -0700

    Enter button doesn't trigger navigation in navbar

--- components/header.tsx
+++ components/header.tsx
@@ -19,7 +19,7 @@ export const StyledLink = styled.a`
     color: black;
   }
 
-  &:hover {
+  &:hover, &:focus {
     color: #B7A57A;
     transition: .25s;
   }
@@ -30,24 +30,28 @@ const Header: React.FC<{}> = (props) => {
 
   const links: React.ReactNode[] = [
     <NavLink key="/" href="/">
-      <StyledLink style={{ fontFamily: "Encode Sans" }}>HOME</StyledLink>
+      <StyledLink style={{ fontFamily: "Encode Sans" }} tabIndex={2}>HOME</StyledLink>
     </NavLink>,
     <span style={{ fontSize: "35px", color: "grey", marginTop: "8px" }}>/</span>,
-    <a key="training" style={{marginTop:"1em", textDecoration:"none"}} href="https://canvas.uw.edu/courses/1176739/pages/service-learning-skills-training-modules?module_item_id=11110569" target="_blank">
-      <StyledLink style={{ fontFamily: "Encode Sans" }}>TRAINING</StyledLink>
+    <a key="training" style={{marginTop:"1em", textDecoration:"none"}}
+       href="https://canvas.uw.edu/courses/1176739/pages/service-learning-skills-training-modules?module_item_id=11110569"
+       target="_blank" tabIndex={-1}>
+      <StyledLink style={{ fontFamily: "Encode Sans" }} tabIndex={3}>TRAINING</StyledLink>
     </a>,
     //*NOTE: Resources name was changed to Links*/
     <span style={{ fontSize: "35px", color: "grey", marginTop: "8px" }}>/</span>,
     <NavLink key="resources" href="/resources">
-      <StyledLink style={{ fontFamily: "Encode Sans" }}>LINKS</StyledLink>
+      <StyledLink style={{ fontFamily: "Encode Sans" }} tabIndex={4}>LINKS</StyledLink>
     </NavLink>,
     <span style={{ fontSize: "35px", color: "grey", marginTop: "8px" }}>/</span>,
-    <a key="protocols" style={{marginTop:"1em", textDecoration:"none", fontFamily: "Encode Sans"}} href="https://canvas.uw.edu/courses/1176739/pages/protocols?module_item_id=15194947" target="_blank">
-      <StyledLink style={{ fontFamily: "Encode Sans" }}>PROTOCOLS</StyledLink>
+    <a key="protocols" style={{marginTop:"1em", textDecoration:"none", fontFamily: "Encode Sans"}}
+       href="https://canvas.uw.edu/courses/1176739/pages/protocols?module_item_id=15194947"
+       target="_blank" tabIndex={-1}>
+      <StyledLink style={{ fontFamily: "Encode Sans" }} tabIndex={5}>PROTOCOLS</StyledLink>
     </a>,
     <span style={{ fontSize: "35px", color: "grey", marginTop: "8px" }}>/</span>,
     <NavLink key="donations" href="/donations">
-      <StyledLink style={{ fontFamily: "Encode Sans" }}>DONATIONS</StyledLink>
+      <StyledLink style={{ fontFamily: "Encode Sans" }} tabIndex={6}>DONATIONS</StyledLink>
     </NavLink>,
     <span style={{ fontSize: "35px", color: "grey", marginTop: "8px" }}>/</span>,
     // sign in and out
@@ -58,6 +62,7 @@ const Header: React.FC<{}> = (props) => {
           firebaseClient.auth().signOut();
         }}
         style={{ fontFamily: "Encode Sans" }}
+        tabIndex={7}
       >
         SIGN OUT
       </StyledLink>
@@ -73,6 +78,7 @@ const Header: React.FC<{}> = (props) => {
           firebaseClient.auth().signInWithPopup(provider);
         }}
         style={{ fontFamily: "Encode Sans" }}
+        tabIndex={7}
       >
         SIGN IN
       </StyledLink>
@@ -98,23 +104,24 @@ const Header: React.FC<{}> = (props) => {
     },
   }));
 
-
   return (
     <div className={useStyles().root}>
       <Link href="/">
-        <img
-          src="/header-logo.png"
-          style={{
-            position: "relative",
-            cursor: "pointer",
-            width: "25em",
-            minWidth: 5,
-            paddingBottom: "10px",
-            paddingLeft: "10px",
-            paddingTop: "5px",
-            marginBottom: "10px"
-          }}
-        />
+        <a tabIndex={1}>
+          <img
+            src="/header-logo.png"
+            style={{
+              position: "relative",
+              cursor: "pointer",
+              width: "25em",
+              minWidth: 5,
+              paddingBottom: "10px",
+              paddingLeft: "10px",
+              paddingTop: "5px",
+              marginBottom: "10px"
+            }}
+          />
+        </a>
       </Link>
 
       <Hidden only={["lg", "md", "xl"]}>
--- components/navlink.tsx
+++ components/navlink.tsx
@@ -19,5 +19,5 @@ export default ({ href, children }: { href: string; children: any }) => {
     className = `${className} ${classes.active}`;
   }
 
-  return <Link href={href}>{React.cloneElement(children, { className })}</Link>;
+  return <Link href={href}><a>{React.cloneElement(children, { className })}</a></Link>;
 };
