1fb89b31bfeb2e33c053f047b68046e5092e4513
Santiago Garcia <sagaan@outlook.com>
2025-01-14 20:28:49 -0800

    update fb admin to use env vars

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -4,9 +4,9 @@ import serviceAccount from "./secret.json";
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: serviceAccount.private_key,
-      clientEmail: serviceAccount.client_email,
-      projectId: serviceAccount.project_id,
+      privateKey: process.env.FB_PRIVATE_KEY,
+      clientEmail: process.env.FB_CLIENT_EMAIL,
+      projectId: process.env.FB_PROJECT_ID,
     }),
     databaseURL: "https://sign-up-9453b.firebaseio.com",
   });
