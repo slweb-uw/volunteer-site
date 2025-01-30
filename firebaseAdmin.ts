45c111a6dcfa80b9e53a55551f3022fbc794dcb8
Santiago Garcia <sagaan@outlook.com>
2025-01-29 18:35:35 -0800

    add firebase emulator configuration and update environment variables

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,16 +1,15 @@
-import firebaseAdmin from "firebase-admin"
-import { ServiceAccount } from "firebase-admin"
+import firebaseAdmin, { type ServiceAccount } from "firebase-admin";
 
 const certification = {
   privateKey: process.env.FB_PRIVATE_KEY,
   clientEmail: process.env.FB_CLIENT_EMAIL,
   projectId: process.env.FB_PROJECT_ID,
-} satisfies ServiceAccount
+} satisfies ServiceAccount;
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(certification),
-  })
+  });
 }
 
-export { firebaseAdmin }
+export { firebaseAdmin };
--- firebaseClient.ts
+++ firebaseClient.ts
@@ -1,11 +1,12 @@
-import { initializeApp, getApps, FirebaseOptions } from "firebase/app";
+import { initializeApp, FirebaseOptions } from "firebase/app";
 import {
   getAuth,
   setPersistence,
   browserLocalPersistence,
+  connectAuthEmulator,
 } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { connectStorageEmulator, getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const CLIENT_CONFIG = {
@@ -17,7 +18,6 @@ const CLIENT_CONFIG = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
 } satisfies FirebaseOptions;
 
-
 const app = initializeApp(CLIENT_CONFIG);
 const auth = getAuth(app);
 const db = getFirestore(app);
@@ -25,5 +25,12 @@ const storage = getStorage(app);
 const getAppAnalytics = () => getAnalytics(app);
 // const analytics = getAnalytics(app);
 setPersistence(auth, browserLocalPersistence);
+
+if (process.env.NEXT_PUBLIC_EMULATE === "true") {
+  console.log("connected to emulators")
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
+  connectStorageEmulator(storage, "127.0.0.1", 9199);
+}
 
 export { auth, db, storage, getAppAnalytics };
