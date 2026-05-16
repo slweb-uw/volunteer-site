bedd57f1bbf6cd82bfb8c5ca98ac045b36f17b50
Sneha <snehamur@cs.washington.edu>
2026-05-15 22:07:33 -0700

    starting on the backend logic for admins ONLY viewing past event attendees

---
+++ pages/calendar/attendees.tsx
@@ -0,0 +1,33 @@
+import { firebaseAdmin } from "firebaseAdmin";
+import { useAuth } from "auth";
+import React, { useState } from "react";
+import { GetServerSideProps } from "next";
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const { event } = ctx.params ?? {};
+  
+    if (!event || typeof event !== "string") {
+      return {
+        notFound: true,
+      };
+    }
+  
+    const eventRef = firebaseAdmin.firestore().collection("events").doc(event);
+    const eventDoc = await eventRef.get();
+  
+    if (!eventDoc.exists) {
+      return { notFound: true };
+    }
+  
+    const volunteers = await eventRef.collection("volunteers").get();
+    const attendees = volunteers.docs.map((doc) => ({
+        docId: doc.id,
+        ...doc.data(),
+    }));
+
+    return { props: { eventName: eventDoc.data()?.name ?? "Event", 
+        attendees
+    } };
+
+}
