a2af9e25db924733e56fc2704ed7c44c0532d9b6
Sneha <snehamur@cs.washington.edu>
2026-05-18 22:02:59 -0700

    server side authenticatation now working with firestore

--- pages/calendar/[event]/attendees.tsx
+++ pages/calendar/[event]/attendees.tsx
@@ -1,9 +1,17 @@
 import { firebaseAdmin } from "firebaseAdmin";
-import { useAuth } from "auth";
 import React, { useState } from "react";
 import { GetServerSideProps } from "next";
 import {VolunteerData} from "new-types"
-
+import TableRow from '@mui/material/TableRow';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import Table from "@mui/material/Table";
+import Paper from "@mui/material/Paper";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import { useAuth } from "auth";
+import AuthorizationMessage from "pages/AuthorizationMessage";
+import nookies from "nookies";
 type Attendee = VolunteerData & {
     docId: string;
 };
@@ -11,11 +19,48 @@ type Attendee = VolunteerData & {
 type ViewingAttendeesPageProps = {
     attendees: Attendee[];
     eventName: string;
+    authenticatedUser: boolean;
 }
+const authorizedViewAttendees = async (email:string | null): Promise<boolean> => {
+    const firestore = firebaseAdmin.firestore();
+    const authAdmin = await firestore.collection("Admins").where("email", "==", email).get();
+    const authLead = await firestore.collection("Leads").where("email", "==", email).get();
+    return !authAdmin.empty || !authLead.empty;
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
+};
+
+
+const getCurrentUserEmail = async (
+    ctx: Parameters<GetServerSideProps>[0],
+  ): Promise<string | null> => {
+
+    const cookies = nookies.get(ctx); 
+    try{
+        const decodedToken = await firebaseAdmin.auth().verifyIdToken(cookies.token); 
+        const email =  decodedToken.email; 
+        console.log("email successfully verified");
+        return email ?? null;
+    } catch(error) {
+        console.error("Could not authenticate current user");
+        return null;
+    }
+}
+export const getServerSideProps: GetServerSideProps<ViewingAttendeesPageProps> = async (ctx) => {
     const { event } = ctx.params ?? {};
-  
+
+    const email = await getCurrentUserEmail(ctx); 
+    const authenticatedUser = await authorizedViewAttendees(email);
+    if (!authenticatedUser) {
+        return {
+            props: {
+                eventName: "", 
+                attendees: [], 
+                authenticatedUser: false
+            }
+        };
+    }
+    console.log("user authenticated");
+
     if (!event || typeof event !== "string") {
       return {
         notFound: true,
@@ -33,19 +78,74 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const attendees = volunteers.docs.map((doc) => ({
         docId: doc.id,
         ...doc.data(),
-    }));
+    })) as Attendee[];
 
     return { 
         props: { eventName: eventDoc.data()?.name ?? "Event", 
-        attendees: attendees,
+        attendees: attendees, authenticatedUser: authenticatedUser
         } 
     };
+
+
 };
-const ViewingAttendeesPage = ({ eventName, attendees }: ViewingAttendeesPageProps) => {
+
+
+const ViewingAttendeesPage = ({ eventName, attendees, authenticatedUser }: ViewingAttendeesPageProps) => {
+    
+
+    const { user, isAdmin, isLead } = useAuth();
+
+    if (!isAdmin && !isLead) {
+        return <AuthorizationMessage user={user} />;
+    }
+    if (!authenticatedUser) {
+        return <AuthorizationMessage user={user} />;
+    }
+
     return (
         <main>
             <h1>{eventName} Attendees</h1>
-            <pre> {JSON.stringify(attendees, null, 2)}</pre>
+            <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Email</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Phone Number</TableCell>
+            <TableCell align="right">Student Discipline</TableCell>
+            <TableCell align="right">Comments</TableCell>
+            <TableCell align="right">Role</TableCell>
+            <TableCell align="right">Date</TableCell>
+
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {attendees.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.email}
+              </TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="right">{row.phoneNumber}</TableCell>
+              <TableCell align="right">{row.studentDiscipline}</TableCell>
+              <TableCell align="right">{row.comments}</TableCell>
+              <TableCell align="right">{row.role}</TableCell>
+              <TableCell align="right">{row.date}</TableCell>
+
+
+
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+
+
+            {/* <pre> {JSON.stringify(attendees, null, 2)}</pre> */}
         </main>
     );
 };
